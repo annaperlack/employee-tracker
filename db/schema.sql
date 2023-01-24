2beee7ed83c8cf97355237bed6661266b84e913f
Anna Perlack <annaperlack@gmail.com>
2023-01-24 17:47:57 -0500

    updated schema, fixed errors

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,21 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY(id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
-    title INT,
+    title VARCHAR(30),
     salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -26,4 +26,4 @@ CREATE TABLE employee (
     PRIMARY KEY(id)
 );
 
-SHOW TABLES();
+SHOW TABLES;
