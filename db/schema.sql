332bc00eedd107b5a60ba57db6e384b4c86e6955
Anna Perlack <annaperlack@gmail.com>
2023-01-23 19:20:45 -0500

    updated gitignore file, created tables, started seed sql

--- db/schema.sql
+++ db/schema.sql
@@ -14,3 +14,11 @@ CREATE TABLE roles (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO departments (id, department_name)
+VALUES ('001', ''),
+       ('002', ),
+      
