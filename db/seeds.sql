d69481b0f18f13ed0cefb13e44e5152ed1af7b17
Anna Perlack <annaperlack@gmail.com>
2023-01-24 17:48:19 -0500

    added seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,14 @@
 INSERT INTO departments (id, department_name)
-VALUES ('001', ''),
-       ('002', ),
+VALUES (001, 'Human Medical Staff'),
+       (002, 'Nurses'),
+       (003, 'Allied Health Professionals'),
+       (004, 'Support Staff');
+
+INSERT INTO roles (id, title, salary, department_id) 
+VALUES (001, 'Surgeon', 300000, 001),
+       (002, 'Nurse Practitioner', 200000, 001);
+        
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (001, 'Robert', 'Smith', 001, 001);
       
